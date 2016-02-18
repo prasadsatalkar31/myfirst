4e85691f49ce5eba4be81abbd2e2103dd4a4246c
prasadpict31 <prasadpict31@gmail.com>
2016-02-18 13:57:35 +0530

    deve

--- round1/views.py
+++ round1/views.py
@@ -5,9 +5,11 @@ from .models import Signup, Questions
 import csv,random
 
 # Create your views here.
+m=0
 name = ""
 mylist=[]
 mylist2=""
+original_ans=""
 
 def db():
     s=settings.BASE_DIR+'/static/questions.csv'
@@ -29,51 +31,68 @@ def index(request):
 
 def signup(request):
     if request.POST.get("login-button"):
-		global name
-		global mylist
-		name = request.POST.get("username")
-		password = request.POST.get("password")
+        global name
+        global m
+        global mylist
+        global original_ans
+        name = request.POST.get("username")
+        password = request.POST.get("password")
 
-		#print name,password
-		s= Signup.objects.filter(userid=name,password=password)
-		if s:
-			uname=name
-			so=Signup.objects.get(userid=uname)
-			mylist=map(str,so.array.split(';'))
-			if mylist[0]=='':
-				return HttpResponseRedirect('/')
-			#print mylist
-			mlist=map(int,mylist)
-			#print mlist
-			value = random.choice(mlist)
-			mlist.remove(value)
-			#print value
-			#print mlist
-			mylist2=";".join(str(x) for x in mlist)
-			so.array=mylist2
-			so.save()
-			#print so.array
-			q=Questions.objects.get(id=value)
-			#print so.userid
-			con={
-			    "q":q,
-			    "s":so,
+        #print name,password
+        s= Signup.objects.filter(userid=name,password=password)
+        if s:
+            uname=name
+            so=Signup.objects.get(userid=uname)
+            mylist=map(str,so.array.split(';'))
+            if mylist[0]=='':
+                return HttpResponseRedirect('/')
+            #print mylist
+            mlist=map(int,mylist)
+            #print mlist
+            m=so.score
+            #print "score is",m
+            value = random.choice(mlist)
+            mlist.remove(value)
+            #print value
+            #print mlist
+            mylist2=";".join(str(x) for x in mlist)
+            so.array=mylist2
+            so.save()
+            q=Questions.objects.get(id=value)
+            original_ans= q.ans
+            print "original_ans", original_ans
+            con={
+                "q":q,
+                "s":so,
                 "uid":name
-			}
-			return render(request,'round1/questions.html',con)
-		else:
-			return HttpResponse("Invalid")
+            }
+            return render(request,'round1/questions.html',con)
+        else:
+            return HttpResponse("Invalid")
     else:
         return render(request, "round1/signup.html",{})
 
 def questions(request):
     global mylist
     global name
+    global m
+    global original_ans
     con={}
     so=Signup.objects.get(userid=name)
     con['s']=so
     con['uid']=name
     if request.POST.get('next'):
+        selected_option=request.POST.get("option")
+        print selected_option
+        if selected_option==original_ans:
+            print "previous",m
+            m+=2
+            print "after",m
+        else:
+            print "previous",m
+            m-=1
+            print "after",m
+
         mylist=map(str,so.array.split(';'))
         if mylist[0]=='':
             return HttpResponseRedirect('/')
@@ -90,6 +109,11 @@ def questions(request):
         so.save()
         #print so.array
         q=Questions.objects.get(id=value)
+        original_ans= q.ans
+        print "original_ans",original_ans
+        so.score=m
+        so.save()
+        #print "score is",m
         con['q']=q
         #print q
         return render(request,"round1/questions.html",con)
@@ -97,14 +121,14 @@ def questions(request):
         return render(request,"round1/questions.html",con)
 
 def update_state(request):
-	if request.method == "POST":
-		if request.is_ajax():
-			uid = request.POST.get("uid1")
+    if request.method == "POST":
+        if request.is_ajax():
+            uid = request.POST.get("uid1")
 
-			s=Signup.objects.get(userid=uid)
-			s.timestamp = s.timestamp-1
-			s.save()
+            s=Signup.objects.get(userid=uid)
+            s.timestamp = s.timestamp-1
+            s.save()
 
-			return questions(request)
-	else:
-		return questions(request)
+            return questions(request)
+    else:
+        return questions(request)
